7dfb85fb03959733e3a1cdebc0b4f65bae8df2d5
yhtiyar <yhtiyarsahatow@gmail.com>
2021-12-28 23:19:14 +0300

    now lexer will return

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,7 +15,14 @@ enum Token {
     Identifier(String),
     Number(f64),
     String(String),
+    Bool(bool),
     EOF,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum LexerError {
+    InvalidCharacter(char),
+    InvalidNumber(String),
 }
 
 impl Lexer {
@@ -51,7 +58,7 @@ impl Lexer {
         }
     }
 
-    fn read_identifier(&mut self) -> String {
+    fn read_identifier(&mut self) -> Result<String, LexerError> {
         let mut result = String::new();
         while let Some(c) = self.ch {
             if !c.is_whitespace() {
@@ -61,10 +68,10 @@ impl Lexer {
                 break;
             }
         }
-        result
+        Ok(result)
     }
 
-    fn read_number(&mut self) -> f64 {
+    fn read_number(&mut self) -> Result<f64, LexerError> {
         let mut result = String::new();
         while let Some(c) = self.ch {
             if c.is_numeric() || c == '.' {
@@ -75,8 +82,11 @@ impl Lexer {
             }
         }
         match result.parse::<f64>() {
-            Ok(n) => n,
-            Err(_) => panic!("Error parsing number : {}", result),
+            Ok(n) => Ok(n),
+            Err(_) => Err(LexerError::InvalidNumber(format!(
+                "Error parsing number : {}",
+                result,
+            ))),
         }
     }
 
@@ -95,71 +105,93 @@ impl Lexer {
         result
     }
 
-    fn next_token(&mut self) -> Token {
+    fn next_token(&mut self) -> Result<Token, LexerError> {
         self.skip_whitespace();
         let ch = match self.ch {
             Some(c) => c,
             None => {
-                return Token::EOF;
+                return Ok(Token::EOF);
             }
         };
 
         match ch {
             '(' => {
                 self.read_char();
-                Token::OpenParen
+                Ok(Token::OpenParen)
             }
             ')' => {
                 self.read_char();
-                Token::CloseParen
+                Ok(Token::CloseParen)
             }
             '[' => {
                 self.read_char();
-                Token::OpenBracket
+                Ok(Token::OpenBracket)
             }
             ']' => {
                 self.read_char();
-                Token::CloseBracket
+                Ok(Token::CloseBracket)
             }
             ',' => {
                 self.read_char();
-                Token::Comma
+                Ok(Token::Comma)
             }
             '.' => {
                 self.read_char();
-                Token::Dot
+                Ok(Token::Dot)
             }
             '-' => {
                 self.read_char();
-                Token::Number(-self.read_number())
+                match self.read_number() {
+                    Ok(n) => Ok(Token::Number(-n)),
+                    Err(e) => Err(e),
+                }
             }
             '"' => {
                 let s = self.read_string();
-                Token::String(s)
+                Ok(Token::String(s))
             }
             _ => {
                 if ch.is_numeric() {
                     let n = self.read_number();
-                    Token::Number(n)
+                    match n {
+                        Ok(n) => Ok(Token::Number(n)),
+                        Err(e) => Err(e),
+                    }
                 } else {
                     let ident = self.read_identifier();
-                    Token::Identifier(ident)
+                    match ident {
+                        Ok(ident) => {
+                            if ident == "true" {
+                                Ok(Token::Bool(true))
+                            } else if ident == "false" {
+                                Ok(Token::Bool(false))
+                            } else {
+                                Ok(Token::Identifier(ident))
+                            }
+                        }
+                        Err(e) => Err(e),
+                    }
                 }
             }
         }
     }
 
-    pub fn tokenize(&mut self) -> Vec<Token> {
+    pub fn tokenize(&mut self) -> Result<Vec<Token>, LexerError> {
         let mut tokens = Vec::new();
         loop {
             let tok = self.next_token();
-            if tok == Token::EOF {
-                tokens.push(tok);
-                break;
+            match tok {
+                Ok(tok) => {
+                    if tok == Token::EOF {
+                        tokens.push(tok);
+                        break;
+                    }
+                    tokens.push(tok)
+                }
+                Err(e) => return Err(e),
             }
-            tokens.push(tok);
         }
-        tokens
+        Ok(tokens)
     }
 }
 
@@ -172,127 +204,129 @@ mod tests {
     fn test_lexer_new() {
         let input = String::from("(+ -1.2 2)");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::OpenParen);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("+")));
-        assert_eq!(l.next_token(), Token::Number(-1.2));
-        assert_eq!(l.next_token(), Token::Number(2.0));
-        assert_eq!(l.next_token(), Token::CloseParen);
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::OpenParen));
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("+"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(-1.2)));
+        assert_eq!(l.next_token(), Ok(Token::Number(2.0)));
+        assert_eq!(l.next_token(), Ok(Token::CloseParen));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_string() {
         let input = String::from("\"hello\"");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::String(String::from("hello")));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::String(String::from("hello"))));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_number() {
         let input = String::from("-1.2");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::Number(-1.2));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::Number(-1.2)));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_identifier() {
         let input = String::from("hello");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("hello")));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("hello"))));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_identifier_with_number() {
         let input = String::from("hello1");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("hello1")));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(
+            l.next_token(),
+            Ok(Token::Identifier(String::from("hello1")))
+        );
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_identifier_with_space() {
         let input = String::from("hello 1");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("hello")));
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("hello"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_identifier_with_space_and_number() {
         let input = String::from("hello 1");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("hello")));
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("hello"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_identifier_with_space_and_number_and_space() {
         let input = String::from("hello 1 2");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("hello")));
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::Number(2.0));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("hello"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::Number(2.0)));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_with_paranthesis() {
         let input = String::from("(+ 1 2)");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::OpenParen);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("+")));
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::Number(2.0));
-        assert_eq!(l.next_token(), Token::CloseParen);
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::OpenParen));
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("+"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::Number(2.0)));
+        assert_eq!(l.next_token(), Ok(Token::CloseParen));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_with_paranthesis_and_space() {
         let input = String::from("( + 1 2 ) ");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::OpenParen);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("+")));
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::Number(2.0));
-        assert_eq!(l.next_token(), Token::CloseParen);
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::OpenParen));
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("+"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::Number(2.0)));
+        assert_eq!(l.next_token(), Ok(Token::CloseParen));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_with_paranthesis_and_space_and_number() {
         let input = String::from("( + 1 2 ) 3");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::OpenParen);
-        assert_eq!(l.next_token(), Token::Identifier(String::from("+")));
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::Number(2.0));
-        assert_eq!(l.next_token(), Token::CloseParen);
-        assert_eq!(l.next_token(), Token::Number(3.0));
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::OpenParen));
+        assert_eq!(l.next_token(), Ok(Token::Identifier(String::from("+"))));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::Number(2.0)));
+        assert_eq!(l.next_token(), Ok(Token::CloseParen));
+        assert_eq!(l.next_token(), Ok(Token::Number(3.0)));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
     fn test_lexer_with_brackets() {
         let input = String::from("[1 2]");
         let mut l = Lexer::new(input);
-        assert_eq!(l.next_token(), Token::OpenBracket);
-        assert_eq!(l.next_token(), Token::Number(1.0));
-        assert_eq!(l.next_token(), Token::Number(2.0));
-        assert_eq!(l.next_token(), Token::CloseBracket);
-        assert_eq!(l.next_token(), Token::EOF);
+        assert_eq!(l.next_token(), Ok(Token::OpenBracket));
+        assert_eq!(l.next_token(), Ok(Token::Number(1.0)));
+        assert_eq!(l.next_token(), Ok(Token::Number(2.0)));
+        assert_eq!(l.next_token(), Ok(Token::CloseBracket));
+        assert_eq!(l.next_token(), Ok(Token::EOF));
     }
 
     #[test]
-    #[should_panic]
     fn test_lexer_invalid_number() {
         let input = String::from("1.2.3");
         let mut l = Lexer::new(input);
-        l.next_token();
+        assert!(matches!(l.next_token(), Err(LexerError::InvalidNumber(_))));
     }
 }
